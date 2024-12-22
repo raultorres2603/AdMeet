f771f1d3403fd78d459f339de2736aa8932f1122
Raul Torres <raultorres2603@gmail.com>
2024-12-22 13:40:43 +0100

    Cambios en menú

--- src/app/Components/nav/navbar/navbar.component.ts
+++ src/app/Components/nav/navbar/navbar.component.ts
@@ -1,14 +1,15 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal, WritableSignal} from '@angular/core';
 import {Iauthservice} from '../../../Interfaces/iauthservice';
 import {AuthService} from '../../../Services/auth/auth.service';
 import {Router, RouterLink} from '@angular/router';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faBars, faCircleUser, faSignOut} from '@fortawesome/free-solid-svg-icons';
+import {Drawer} from 'primeng/drawer';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [FontAwesomeModule, RouterLink],
+  imports: [FontAwesomeModule, RouterLink, Drawer],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
@@ -16,8 +17,16 @@ export class NavbarComponent {
 
   private authService: Iauthservice = inject(AuthService)
   private router: Router = inject(Router)
-  protected readonly faBars = faBars
   protected readonly faCircleUser = faCircleUser;
+  protected readonly faSignOut = faSignOut;
+  protected readonly faBars = faBars;
+  protected readonly items = [
+    {
+      label: 'Profile',
+      icon: 'faCircleUser',
+    }
+  ]
+  protected visible: WritableSignal<boolean> = signal(false)
 
 
   logOut(): void {
@@ -28,5 +37,7 @@ export class NavbarComponent {
     this.router.navigate(['/profile']);
   }
 
-  protected readonly faSignOut = faSignOut;
+  changeVisible(): void {
+    this.visible.set(!this.visible());
+  }
 }
