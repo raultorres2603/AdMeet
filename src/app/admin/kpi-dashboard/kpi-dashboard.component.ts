111a801a4be94349241825fbf85fa8e4febc0545
Raul Torres <55553058+raultorres2603@users.noreply.github.com>
2024-12-30 12:00:44 +0100

    Added 1 more Dashboard. We need to put more things

--- src/app/admin/kpi-dashboard/kpi-dashboard.component.ts
+++ src/app/admin/kpi-dashboard/kpi-dashboard.component.ts
@@ -5,18 +5,22 @@ import {HttpService} from '../../Services/http/http.service';
 import {Ihttp} from '../../Interfaces/ihttp';
 import {IKpiData} from '../../Interfaces/iKpiData';
 import {ToastrService} from 'ngx-toastr';
+import {IkpiDashboard} from '../../Interfaces/ikpi-dashboard';
+import {TableModule} from 'primeng/table';
 
 @Component({
   selector: 'app-kpi-dashboard',
-  imports: [NavbarComponent, ChartModule],
+  imports: [NavbarComponent, ChartModule, TableModule],
   templateUrl: './kpi-dashboard.component.html',
   styleUrl: './kpi-dashboard.component.css'
 })
-export class KpiDashboardComponent implements OnInit {
+export class KpiDashboardComponent implements OnInit, IkpiDashboard {
 
   protected readonly usersData: WritableSignal<any> = signal(null);
+  protected readonly countryData: WritableSignal<any> = signal(null);
   private http: Ihttp = inject(HttpService);
   private toastr: ToastrService = inject(ToastrService);
+  protected loadingCountryTable: WritableSignal<boolean> = signal(false);
   protected readonly options = {
     responsive: true,
     plugins: {
@@ -39,9 +43,14 @@ export class KpiDashboardComponent implements OnInit {
   };
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  private getData(): void {
     this.http.get('api/admin/kpi').subscribe({
       next: (response: IKpiData) => {
         this.usersData.set(this.transformDataOnUsersBar(response));
+        this.countryData.set(this.transformDataOnCountryPie(response));
       },
       error: (err) => {
         console.log(err);
@@ -77,31 +86,90 @@ export class KpiDashboardComponent implements OnInit {
         {
           label: "Usuarios Logeados",
           data: usersLogedInDataset,
-          backgroundColor: "rgba(54, 162, 235, 0.2)",
-          borderColor: "rgba(54, 162, 235, 1)",
+          backgroundColor: this.generateRandomColors(),
+          borderColor: this.generateRandomColors(),
           borderWidth: 1
         },
         {
           label: "Usuarios Registrados",
           data: usersRegisteredDataset,
-          backgroundColor: "rgba(255, 206, 86, 0.2)",
-          borderColor: "rgba(255, 206, 86, 1)",
+          backgroundColor: this.generateRandomColors(),
+          borderColor: this.generateRandomColors(),
           borderWidth: 1
         },
       ]
     };
   };
 
-  protected seeSpecificData(event: any): void {
+  private transformDataOnCountryPie(data: IKpiData) {
+    // Combinar todos los country y eliminar duplicados
+
+    const dataOfBar = {
+      labels: [] as Array<string>,
+      datasets: [] as Array<any>
+    }
+
+    const allCountry = [
+      ...data.usersFromDiffCountry.map((entry: any) => entry.country),
+    ];
+    const labels = Array.from(new Set(allCountry)).sort(); // Ordenar las country
+    dataOfBar.labels = labels;
+    console.log(data.usersFromDiffCountry);
+
+    // Crear dataset para "Usuarios de diferentes paises"
+    const usersFromDiffCountryDataset = labels.map((country) => {
+      const entry = data.usersFromDiffCountry.find((item: any) => item.country === country);
+      return entry ? entry.totalUsers : 0;
+    });
+
+    const colores = [];
+
+    // Generate different colors for each dataset
+    for (const country in labels) {
+      console.log(country);
+      colores.push(this.generateRandomColors());
+    }
+
+    dataOfBar.datasets.push({
+      data: usersFromDiffCountryDataset,
+      backgroundColor: colores,
+      borderColor: colores,
+      borderWidth: 1
+    });
+
+    // Objeto transformado para Chart.js
+    return dataOfBar;
+  }
+
+  private generateRandomColors(): string {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgba(${r}, ${g}, ${b}, 0.5)`; // Color con transparencia
+  }
+
+  protected seeSpecificDataOnUsersBar(event: any): void {
     const datasetIndex = event.element.datasetIndex;
     const dataIndex = event.element.index;
     const dataset = this.usersData().datasets[datasetIndex];
     const label = this.usersData().labels[dataIndex];
     const value = dataset.data[dataIndex];
 
-    console.log(`Dataset: ${dataset.label}`);
     console.log(`Label: ${label}`);
     console.log(`Value: ${value}`);
     // Puedes realizar otras acciones aquí, como actualizar datos o mostrar un modal.
+  }
+
+  protected seeSpecificDataOnCountryGraph(event: any): void {
+    const datasetIndex = event.element.datasetIndex;
+    const dataIndex = event.element.index;
+    const dataset = this.countryData().datasets[datasetIndex];
+    const label = this.countryData().labels[dataIndex];
+    const value = dataset.data[dataIndex];
+
+    console.log(`Label: ${label}`);
+    console.log(`Value: ${value}`);
+    // Puedes realizar otras acciones aquí, como actualizar datos o mostrar un modal.
   }
 }
